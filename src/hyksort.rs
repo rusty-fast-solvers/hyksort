896d4d281edd610c95a0a1f779d7c7de500da063
Srinath Kailasa <srinathkailasa@gmail.com>
2021-12-05 17:52:04 +0000

    Add working all to all kway

--- src/hyksort.rs
+++ src/hyksort.rs
@@ -1,6 +1,9 @@
 use mpi::traits::*;
 use mpi::topology::{UserCommunicator, SystemCommunicator};
 use mpi::collective::{SystemOperation};
+use mpi::datatype::{Partition, PartitionMut};
+use mpi::{Count, Address};
+
 
 // #[cfg(feature = "user-operations")]
 // use libffi::middle::{Cif, Closure, Type};
@@ -12,14 +15,14 @@ pub fn modulo(a: i32, b: i32) -> i32 {
     ((a % b) + b) % b
 }
 
-pub fn hyksort(
-    arr: &mut [i32],
+pub fn all_to_all_kwayv(
+    arr: &mut Vec<i32>,
     mut kway: i32,
-    comm: UserCommunicator
+    mut comm: UserCommunicator
 ) {
 
-    let npes = comm.size();
-    let myrank = comm.rank();
+    let mut npes = comm.size();
+    let mut myrank = comm.rank();
 
     let mut tot_size: i32 = 0;
     let mut nelem: i32 = arr.len() as i32;
@@ -27,11 +30,11 @@ pub fn hyksort(
     comm.all_reduce_into(&nelem, &mut tot_size, SystemOperation::sum());
 
     // Perform local sort
-    arr.sort();
+    // arr.sort();
 
     // Allocate all buffers
     let mut arr_ = vec![0; (nelem*2) as usize];
-    let mut arr__ = vec![0; (nelem*2) as usize];
+    // let mut arr__ = vec![0; (nelem*2) as usize];
 
 
     while (npes > 1 && tot_size>0) {
@@ -57,16 +60,29 @@ pub fn hyksort(
             let mut send_disp: Vec<i32> = vec![0; (kway+1) as usize];
 
             send_disp[0] = 0;
-            send_disp[kway as usize] = arr.len() as i32;
+            // Send whole message every time (no bucketing)
+            // send_disp[kway as usize] = arr.len() as i32;
+            let msg_size = arr.len() as i32;
+            send_disp[kway as usize] = msg_size*kway;
 
-            // Placeholders due to fake split keys
+            // // Placeholders due to fake split keys
+            // for i in 0..kway {
+            //     send_disp[i as usize] = (myrank+1)*i;
+            // }
+
+            // for i in 0..kway {
+            //     send_size[i as usize] = send_disp[(i+1) as usize]-send_disp[i as usize];
+            // }
+
             for i in 0..kway {
-                send_disp[i as usize] = (myrank+1)*i;
+                send_size[i as usize] = msg_size;
             }
 
             for i in 0..kway {
-                send_size[i as usize] = send_disp[(i+1) as usize]-send_disp[i as usize];
+                send_disp[i as usize] = i*msg_size;
             }
+
+
             // Testing above code
             // println!("HERE {:?} {:?} {:?}", myrank, send_disp, send_size);
 
@@ -84,7 +100,7 @@ pub fn hyksort(
                 for j in 0..(if i_==0 || i_==kway/2 { 1 }  else {2}) {
                     let i = if i_ == 0  {i1} else { if ((j+blk_id / i_)%2 == 0)  {i1} else {i2} } ;
                     let partner = blk_size*i+new_pid;
-                    // println!("rank {:?} partner : {:?}", myrank, partner);
+                    // println!("rank {:?} partner : {:?} colour {:?}", myrank, partner, blk_id);
                     // println!("rank {:?} i : {:?}", myrank, send_size[i as usize]);
                     let partner_process = comm.process_at_rank(partner);
                     mpi::point_to_point::send_receive_into(
@@ -98,14 +114,122 @@ pub fn hyksort(
                     recv_iter += 1;
                 }
             }
-            println!("RANK {:?} recv_cnt {:?} recv_disp {:?}", myrank, recv_cnt, recv_disp);
+            // println!("RANK {:?} recv_cnt {:?} recv_disp {:?}", myrank, recv_cnt, recv_disp);
+            // break;
 
             // Communicate data
-            break;
+            let asynch_count = 2;
+            recv_iter = 0;
+
+            // Resize buffers
+            arr_.resize(recv_disp[kway as usize] as usize, 0);
+            // arr__.resize(recv_disp[kway as usize] as usize, 0);
+
+            for i_ in 0..=(kway/2) {
+                let i1 = modulo(blk_id+i_, kway);
+                let i2 = modulo(blk_id+kway-i_, kway);
 
 
+                for j in 0..(if i_==0 || i_==kway/2 { 1 }  else {2}) {
+                    let i = if i_ == 0  {i1} else { if ((j+blk_id / i_)%2 == 0)  {i1} else {i2} } ;
+                    let partner = blk_size*i+new_pid;
+                    let partner_process = comm.process_at_rank(partner);
 
-        }
+                    // println!("rank {:?} receices from {:?} and sends to {:?}", myrank, partner, partner);
 
+                    // Receive indices
+                    let r_lidx: usize = recv_disp[recv_iter as usize] as usize;
+                    let r_ridx: usize = r_lidx + recv_size[recv_iter as usize] as usize;
+
+                    // Send indices
+                    let s_lidx: usize = send_disp[i as usize] as usize;
+                    let s_ridx: usize = s_lidx + send_size[i as usize] as usize;
+
+                    mpi::request::scope(|scope| {
+                            // let mut sreq = partner_process.immediate_synchronous_send(scope, &arr[s_lidx..s_ridx]);
+                            let mut sreq = partner_process.immediate_synchronous_send(scope, &arr[..]);
+                            let rreq = partner_process.immediate_receive_into(scope, &mut arr_[r_lidx..r_ridx]);
+                            rreq.wait();
+                            loop {
+                                match sreq.test() {
+                                    Ok(_) => {
+                                        break;
+                                    } Err(req) => {
+                                        sreq = req;
+                                    }
+                                }
+                            }
+                    });
+                    recv_iter += 1;
+                }
+            }
+
+            // Swap buffers
+            std::mem::swap(arr, &mut arr_);
+
+            // Handle communicator
+            {
+                comm = comm.split_by_color(mpi::topology::Color::with_value(blk_id)).unwrap();
+                npes = comm.size();
+                myrank = comm.rank();
+
+            }
+
+         }
     }
+}
+
+
+
+pub fn all_to_all<T>(
+    world: SystemCommunicator,
+    size: mpi::topology::Rank,
+    buckets: Vec<Vec<T>>) -> Vec<T>
+where T: Default+Clone+Equivalence
+{
+
+    let mut counts_snd: Vec<Count> = vec![0; size as usize];
+
+    for (i, bucket) in buckets.iter().enumerate() {
+        counts_snd[i] = bucket.len() as Count;
+    }
+
+    // Flatten buckets, after bucketing
+    let buckets_flat: Vec<T> = buckets.into_iter().flatten().collect();
+
+    let displs_snd: Vec<Count> = counts_snd
+        .iter()
+        .scan(0, |acc, &x| {
+            let tmp = *acc;
+            *acc += x;
+            Some(tmp)
+        })
+        .collect();
+
+    // All to All for bucket sizes
+    let mut counts_recv: Vec<Count> = vec![0; size as usize];
+
+    world.all_to_all_into(&counts_snd[..], &mut counts_recv[..]);
+
+    // displacements
+    let displs_recv: Vec<Count> = counts_recv
+        .iter()
+        .scan(0, |acc, &x| {
+            let tmp = *acc;
+            *acc += x;
+            Some(tmp)
+        })
+        .collect();
+
+    // Allocate a buffer to receive relevant data from all processes.
+    let total: Count = counts_recv.iter().sum();
+    let mut received = vec![T::default(); total as usize];
+    let mut partition_receive = PartitionMut::new(&mut received[..], counts_recv, &displs_recv[..]);
+
+    // Allocate a partition of the data to send to each process
+    let partition_snd = Partition::new(&buckets_flat[..], counts_snd, &displs_snd[..]);
+
+    world.all_to_all_varcount_into(&partition_snd, &mut partition_receive);
+
+    received
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,54 @@
+use std::time::Instant;
+use std::collections::HashMap;
+
 use mpi::traits::*;
-use hyksort::hyksort;
 use mpi::topology::{Color};
+
+use hyksort::hyksort::{all_to_all_kwayv, all_to_all};
+
+pub type Times = HashMap<String, u128>;
+
 
 fn main() {
 
     let universe = mpi::initialize().unwrap();
     let world = universe.world();
     let world = world.split_by_color(Color::with_value(0)).unwrap();
+    let size = world.size();
     let rank = world.rank();
-    let kway = 4;
+    let kway = 2;
 
-    let mut arr = vec![rank; (kway*(rank+1)) as usize];
-    hyksort::hyksort(&mut arr, kway, world);
+    // let mut arr = vec![rank; (kway*(rank+1)) as usize];
+    // let mut arr = vec![rank; (rank+1) as usize];
+    let mut arr = vec![rank; (1e6) as usize];
+    // all_to_all_kwayv(&mut arr, kway, world);
+
+    let mut buckets: Vec<Vec<i32>> = vec![Vec::new(); (size-1) as usize];
+
+    for i in 0..(size-1) {
+        for elem in &arr {
+            buckets[i as usize].push(elem.clone());
+        }
+    }
+
+    let mut times: Times = HashMap::new();
+
+    let kwayt = Instant::now();
+    all_to_all_kwayv(&mut arr, kway, world);
+    times.insert("kway".to_string(), kwayt.elapsed().as_millis());
+
+    let world = universe.world();
+    let intrinsic = Instant::now();
+    all_to_all(world, size, buckets);
+    times.insert("intrinsic".to_string(), intrinsic.elapsed().as_millis());
+
+    if rank == 0 {
+        println!(
+            "{:?}, {:?}, {:?}",
+            size,
+            times.get(&"kway".to_string()).unwrap(),
+            times.get(&"intrinsic".to_string()).unwrap()
+        );
+        // println!("A: {:?} B: {:?}", a.len(), b.len());
+    }
 }
